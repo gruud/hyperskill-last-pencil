65da64d3d6c8a170957b7be1cdfb8315f081472b
gruud <6158418+gruud@users.noreply.github.com>
2023-12-11 23:04:03 +0100

    Project Step 1

---
+++ last_pencil.py
@@ -0,0 +1,11 @@
+"""Last Pencil Hyperskill Project implementation."""
+
+
+def main():
+    """Main program"""
+    print("||||||||")
+    print("Your turn!")
+
+
+if __name__ == "__main__":
+    main()
