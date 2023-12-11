bb691c785ff128ff9f0eb44eab79762ed37ad2c4
gruud <6158418+gruud@users.noreply.github.com>
2023-12-11 23:10:58 +0100

    Project Step 2

--- last_pencil.py
+++ last_pencil.py
@@ -1,10 +1,23 @@
 """Last Pencil Hyperskill Project implementation."""
 
 
+def print_pencils(pencils_number):
+    """Print a pencil <pencils_number> times"""
+    print("|" * pencils_number)
+
+
+def print_start_player(player_name):
+    """Print an engaging sentence for the starting player"""
+    print(f"{player_name} is going first!")
+
+
 def main():
     """Main program"""
-    print("||||||||")
-    print("Your turn!")
+    pencils_number = int(input("How many pencils would you like to use:"))
+    first_player = input("Who will be the first (John, Jack):")
+
+    print_pencils(pencils_number)
+    print_start_player(first_player)
 
 
 if __name__ == "__main__":
