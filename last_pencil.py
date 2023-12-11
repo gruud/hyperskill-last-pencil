85be65703de1d5896e85722284f50ad7c72d8101
gruud <6158418+gruud@users.noreply.github.com>
2023-12-11 23:24:07 +0100

    Project Step 3

--- last_pencil.py
+++ last_pencil.py
@@ -6,18 +6,27 @@ def print_pencils(pencils_number):
     print("|" * pencils_number)
 
 
-def print_start_player(player_name):
-    """Print an engaging sentence for the starting player"""
-    print(f"{player_name} is going first!")
+def get_player_input(player_name):
+    """Get the next player number of pencils to remove"""
+    return int(input(f"{player_name}'s turn:"))
 
 
 def main():
     """Main program"""
-    pencils_number = int(input("How many pencils would you like to use:"))
-    first_player = input("Who will be the first (John, Jack):")
 
-    print_pencils(pencils_number)
-    print_start_player(first_player)
+    # Use a players' list to easily iterate over the players
+    players = ["John", "Jack"]
+    pencils_number = int(input("How many pencils would you like to use:"))
+    starting_player = input(f"Who will be the first ({players[0]}, {players[1]}):")
+
+    next_player = players.index(starting_player)
+
+    while pencils_number > 0:
+        print_pencils(pencils_number)
+        pencils_to_remove = get_player_input(players[next_player])
+
+        pencils_number -= pencils_to_remove
+        next_player = (next_player + 1) % len(players)
 
 
 if __name__ == "__main__":
